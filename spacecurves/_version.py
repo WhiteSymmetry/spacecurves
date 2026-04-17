9f06f1159aec63ec739014397d8abeafc8e0970a
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2026-04-17 22:43:28 +0000

    Add files via upload

---
+++ spacecurves/_version.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# _version.py
+
+__version__ = "0.1.0"
+__license__ = "AGPL-3.0-or-later"
+__description__ = "Space Curves (spacecurves, Uzay Eğrileri): Uzay Dolduran Eğriler Modülü"
+__author__ = "Mehmet Keçeci"
+__url__ = "https://github.com/WhiteSymmetry/spacecurves"
+__docs__ = "https://github.com/WhiteSymmetry/spacecurves"  # Opsiyonel: Dokümantasyon linki
+__dependencies__ = ["python>=3.11"]  # Diğer bağımlılıkları da ekleyebilirsiniz
