f5bf91de2abd5ee94cab9fc388967d148a519c2a
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2026-08-21 15:43:47 +0300

    Update author and maintainer email addresses

--- setup.py
+++ setup.py
@@ -34,8 +34,8 @@ setup(
     long_description_content_type="text/markdown",
     author="Mehmet Keçeci",
     maintainer="Mehmet Keçeci",
-    author_email="mkececi@yaani.com",
-    maintainer_email="mkececi@yaani.com",
+    author_email="enfo@tuta.io",
+    maintainer_email="enfo@tuta.io",
     url="https://github.com/WhiteSymmetry/spacecurves",
     packages=find_packages(),
     package_data={
